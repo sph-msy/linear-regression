a1a2fcb7f244d839257c6922953e743be8b97e6a
Sph-Msy <git935@susy-rose.fr>
2025-09-25 09:59:40 +0200

    Filling the missing values with median or mode

--- linear_regression.py
+++ linear_regression.py
@@ -27,37 +27,109 @@ def count_missing_values_rows(data):
             null_val_row_count += 1
             # print(data.iloc[i])
 
-    print("Rows with missing values:",null_val_row_count,"/",len(data))
+    print("Number of rows with missing values:",null_val_row_count,"/",len(data))
 
     return null_val_row_count
 
-def visualize_data(table):
+def analyse_and_fill_data(table, heatmap=True, histo=False):
+    """
+    Count the missing values in columns, determine if columns are filled with continuous or discrete data,
+    Replace the missing values by median or mode values depending on the distribution.
 
-    # count the number of rows with missing data
-    count_missing_values_rows(table)
+    Input:
+        table: panda DataFrame containing the whole dataset
+        heatmap (default: True): if True, display the heatmap distribution of the missing value
+        histo (default: False): if True, plot and display the histogram of every column
 
-    # visualize the number of missing values for each columns
-    print(table.isnull().sum())
+    Output:
+        complete_data: panda DataFrame without any missing value anymore (everything is filled)
+    """
 
-    print("Number of columns with missing data:",(table.isnull().sum() != 0).sum(),"/",len(table.columns))
+    # # visualize the number of missing values for each columns
+    # print(table.isnull().sum())
+
+    # print("Number of columns with missing data:",(table.isnull().sum() != 0).sum(),"/",len(table.columns))
 
     # Visualize missing data
-    plt.figure()
-    plt.title("Distribution of the missing values")
-    plt.imshow(table.isnull(), aspect="auto")
-    plt.show()
+    if heatmap:
+        plt.figure()
+        plt.title("Distribution of the missing values")
+        plt.imshow(table.isnull(), aspect="auto")
+        plt.show()
 
+    # we assume from the heatmap than data is MAR
+
+    print("-" * 30)
+
+    # for i in range(len(table.columns)):
+    for column in table.columns:
+
+        # print(f"Column {table.columns[i]} has {table.isnull()[i]} missing values")
+        print(f"Column {column} has {table[column].isnull().sum()} missing values")
+
+        # Visualize the data repartition
+        if histo:
+            plt.figure()
+            # plt.hist(table.iloc[:,i], bins=20, xlabel=f"{table.iloc[0,i]}")
+            table[column].plot.hist(bins=20, legend=True)
+            plt.show()
+        
+
+        # For each column with missing values, check the number of unique values
+        # and depending on the distribution, fill column with median or mode value
+
+        if table[column].isnull().sum()>0:
+            unique_values = table[column].nunique()
+            print(f"Unique values: {unique_values}")
+            
+            # Check if the column is more likely to be discrete or continuous
+            if unique_values < 20:  # You can adjust this threshold
+                print(f"Column '{column}' is likely discrete.")
+                table[column] = table[column].fillna(table[column].mode()[0])
+                print(f"Column '{column}' missing values replaced with mode.")
+
+            else:
+                print(f"Column '{column}' is likely continuous.")
+                # table[column].fillna(table[column].median(), inplace=True)
+                table[column] = table[column].fillna(table[column].median())
+                print(f"Column '{column}' missing values replaced with median value.")
+
+        print("-" * 30)
+
+    # print('test')
+    # print(table.isnull().sum())
+
+    # rename the filled data
+    complete_data = table
+
+    return complete_data
+    
+        
+
+
+        
 
     
 
 def preprocess_data(data):
+
     # print(data.head()) # print the header
 
-    visualize_data(data) # we assume from the heatmap than data is MAR
+    # count the number of rows with missing data
+    nb_missing_val_rows = count_missing_values_rows(data)
+
+    if nb_missing_val_rows>0:
+        complete_data = analyse_and_fill_data(data, heatmap=False)
+
+        # # test
+        # print(complete_data.isnull().sum())
+        
 
     
 
-    pass
+    
+
+    
 
 def main():
     data = import_data("HousingData.csv")
